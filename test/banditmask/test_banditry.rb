ef6681afe3e44da2d3294c07a1c42d7c50614c79
John Parker <jparker@urgetopunt.com>
2015-07-06 18:32:53 -0700

    cosmetic renaming (easier to read)

--- test/banditmask/test_banditry.rb
+++ test/banditmask/test_banditry.rb
@@ -9,62 +9,62 @@ class BanditMask
     end
 
     def setup
-      @cls = Struct.new(:perm_mask) do
+      @cls = Struct.new(:bitmask) do
         extend Banditry
       end
     end
 
     def test_mask_defines_reader
-      refute_respond_to @cls.new, :perms
-      @cls.bandit_mask :perm_mask, as: :perms, with: TestMask
-      assert_respond_to @cls.new, :perms
+      refute_respond_to @cls.new, :bits
+      @cls.bandit_mask :bitmask, as: :bits, with: TestMask
+      assert_respond_to @cls.new, :bits
     end
 
     def test_mask_defines_writer
-      refute_respond_to @cls.new, :perms=
-      @cls.bandit_mask :perm_mask, as: :perms, with: TestMask
-      assert_respond_to @cls.new, :perms=
+      refute_respond_to @cls.new, :bits=
+      @cls.bandit_mask :bitmask, as: :bits, with: TestMask
+      assert_respond_to @cls.new, :bits=
     end
 
     def test_mask_defines_query
-      refute_respond_to @cls.new, :perms?
-      @cls.bandit_mask :perm_mask, as: :perms, with: TestMask
-      assert_respond_to @cls.new, :perms?
+      refute_respond_to @cls.new, :bits?
+      @cls.bandit_mask :bitmask, as: :bits, with: TestMask
+      assert_respond_to @cls.new, :bits?
     end
 
     def test_reader_instantiates_bandit_mask_with_current_bitmask
-      @cls.bandit_mask :perm_mask, as: :perms, with: TestMask
+      @cls.bandit_mask :bitmask, as: :bits, with: TestMask
       obj = @cls.new 0b011
-      assert_equal [:read, :write], obj.perms
+      assert_equal [:read, :write], obj.bits
     end
 
     def test_writer_overwrites_current_bitmask_with_bitmask_for_given_values
-      @cls.bandit_mask :perm_mask, as: :perms, with: TestMask
+      @cls.bandit_mask :bitmask, as: :bits, with: TestMask
       obj = @cls.new 0b011
-      obj.perms = [:read, :execute]
-      assert_equal 0b101, obj.perm_mask
+      obj.bits = [:read, :execute]
+      assert_equal 0b101, obj.bitmask
     end
 
     def test_query_method_with_a_single_bit
-      @cls.bandit_mask :perm_mask, as: :perms, with: TestMask
+      @cls.bandit_mask :bitmask, as: :bits, with: TestMask
       obj = @cls.new 0b001
-      assert obj.perms?(:read), "#{obj.inspect} must have :read"
-      refute obj.perms?(:write), "#{obj.inspect} must NOT have :write"
+      assert obj.bits?(:read), "#{obj.inspect} must have :read"
+      refute obj.bits?(:write), "#{obj.inspect} must NOT have :write"
     end
 
     def test_query_method_with_multiple_bits
-      @cls.bandit_mask :perm_mask, as: :perms, with: TestMask
+      @cls.bandit_mask :bitmask, as: :bits, with: TestMask
       obj = @cls.new 0b011
-      assert obj.perms?(:read, :write), "#{obj.inspect} must have :read and :write"
-      refute obj.perms?(:read, :execute), "#{obj.inspect} must NOT have :read and :execute"
+      assert obj.bits?(:read, :write), "#{obj.inspect} must have :read and :write"
+      refute obj.bits?(:read, :execute), "#{obj.inspect} must NOT have :read and :execute"
     end
 
     def test_query_method_with_undefined_bit
-      @cls.bandit_mask :perm_mask, as: :perms, with: TestMask
+      @cls.bandit_mask :bitmask, as: :bits, with: TestMask
       obj = @cls.new 0b001
 
       assert_raises ArgumentError do
-        obj.perms? :bogus
+        obj.bits? :bogus
       end
     end
   end
